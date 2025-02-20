bf43078d6ace3d7472aadbeee1bfd071a04455ac
vishnuvardhancoder <vishnuvardhan.s.techie@gmail.com>
2025-02-21 00:44:02 +0530

    updated frontend url to backend..

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -130,14 +130,14 @@ export class AuthController {
           // console.log('Redirecting with Profile Image:', profileImage);
 
           // Redirect with necessary parameters, make sure profile image is properly URL encoded
-          const redirectUri = `http://localhost:3001/callback?access_token=${accessToken}&user_id=${internalUserId}&google_user_id=${googleUserId}&username=${username}&profile_image=${encodeURIComponent(profileImage)}`;
+          const redirectUri = `https://remastered-vtm-frontend.vercel.app/callback?access_token=${accessToken}&user_id=${internalUserId}&google_user_id=${googleUserId}&username=${username}&profile_image=${encodeURIComponent(profileImage)}`;
           // console.log('Redirect URL:', redirectUri);
 
           
           return res.redirect(redirectUri);
       } catch (error) {
           console.error('Error during Google authentication:', error);
-          return res.redirect('http://localhost:3001/login?error=authentication_failed');
+          return res.redirect('https://remastered-vtm-frontend.vercel.app/login?error=authentication_failed');
       }
   }
   
