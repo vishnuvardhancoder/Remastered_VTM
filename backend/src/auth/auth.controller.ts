39e92d9a01930a643fe07a08dfea127db39ee2f1
vishnuvardhancoder <vishnuvardhan.s.techie@gmail.com>
2025-02-22 08:31:16 +0530

    updated auth controller.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -131,14 +131,14 @@ export class AuthController {
 
           // Redirect with necessary parameters, make sure profile image is properly URL encoded
           const redirectUri = `
-https://vtaskmanager.vercel.app//callback?access_token=${accessToken}&user_id=${internalUserId}&google_user_id=${googleUserId}&username=${username}&profile_image=${encodeURIComponent(profileImage)}`;
+https://vtaskmanager.vercel.app/callback?access_token=${accessToken}&user_id=${internalUserId}&google_user_id=${googleUserId}&username=${username}&profile_image=${encodeURIComponent(profileImage)}`;
           // console.log('Redirect URL:', redirectUri);
 
           
           return res.redirect(redirectUri);
       } catch (error) {
           console.error('Error during Google authentication:', error);
-          return res.redirect('https://vtaskmanager.vercel.app//login?error=authentication_failed');
+          return res.redirect('https://vtaskmanager.vercel.app/login?error=authentication_failed');
       }
   }
   
